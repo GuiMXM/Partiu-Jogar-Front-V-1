589d57acc073957e8d6e1c5779c7dd392d3f3c5c
joseRibamar21 <jose.21rb@outlook.com>
2022-10-04 18:00:35 -0300

    correção #1

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import { HtmlHTMLAttributes, ReactNode } from "react";
 
 export default function Footer() {
     return (
-        <div className="flex flex-wrap gap-9  bg-[#0C62C4] p-20 justify-between font-[400] text-white text-base lg:text-2xl">
+        <ContinerFooter>
             <div className="flex gap-10 items-center ">
                 <Image height="50px" width="50px" src="/ball.png" />
                 <span>Mais de 20 espaços<br /> para você jogar</span>
@@ -17,6 +18,18 @@ export default function Footer() {
                 <Image height="50px" width="50px" className="w-[20px] h-[20px]" src="/watch.png" />
                 <span>Escolha seus horários<br /> de preferência</span>
             </div>
+        </ContinerFooter>
+    )
+}
+
+interface AuxProps extends HtmlHTMLAttributes<AuxProps> {
+    children: ReactNode;
+  }
+
+function ContinerFooter({children}: AuxProps){
+    return(
+        <div className="flex flex-wrap gap-9  bg-[#0C62C4] p-20 justify-between font-[400] text-white text-base lg:text-2xl">
+            {children}
         </div>
     )
 }
