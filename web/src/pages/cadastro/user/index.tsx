d92a4744816cad917d3ba2c1310b0dd7f91e9ad6
Guimxm <gui2918@gmail.com>
2022-10-05 16:05:14 -0300

    melhoria responsividade- Registration screens

--- web/src/pages/cadastro/user/index.tsx
+++ web/src/pages/cadastro/user/index.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 type Props = {}
 
 const usuário = (props: Props) => {
-  const fieldsetCss = 'border-neutral-300 border-2 rounded-lg items-center w-[350px] focus-within:border-white text-neutral-300 cursor-default focus-within:text-white';
+  const fieldsetCss = 'border-neutral-300 border-2 rounded-lg items-center w-[350px] focus-within:border-white text-neutral-300 cursor-default focus-within:text-white place-self-center';
   return (
     
     <>
       <div className='flex flex-row-reverse items-start md:items-center justify-center h-[90vh]   '>
 
-          <section className='hidden md:flex flex-col relative items-start justify-center h-[90vh] w-1/2 mr-8'>
+          <section className='hidden md:flex flex-col relative items-start justify-center h-[90vh] w-1/2   '>
             <img src="../images/título1.png" alt="Título" className="  w-5/6 win-w-[200px] min-h-[170px]  sm:mt-[50px]"/>
 
             <span className='flex md:w-[400px] h-fit  pl-4 pb-[10px] md:mt-0 text-xl font-medium lg:ml-52 border-l-[16px] border-[#C3F23C] leading-7  sm:mt-[50px] '>
@@ -17,7 +17,7 @@ const usuário = (props: Props) => {
 
           </section>
 
-          <div className='flex items-center place-content-center   place-self-center md:place-self-start lg:mt-20 md:ml-48 h-[650px] w-[480px] min-w-[430px]  bg-[#3D93F5] rounded-xl shadow-gray-600 shadow-lg'>
+          <div className='flex items-center place-content-center   place-self-center md:place-self-start sm:ml-6 md:ml-44 lg:mt-20 h-[620px] w-[480px] min-w-[430px] bg-[#3D93F5] rounded-xl shadow-gray-600  shadow-lg'>
 
            <form className=' relative space-y-4 '>
              <h2 className='text-white text-2xl font-bold tracking-[.2rem] mb-8'>Realize seu cadastro:</h2>
@@ -42,10 +42,6 @@ const usuário = (props: Props) => {
                   <input type="text" placeholder='' className='outline-none bg-transparent ml-2 w-[350px]'/>
              </fieldset>
 
-             <fieldset className={fieldsetCss}> 
-                <legend className='ml-2 text-sm px-1'>Instituição</legend>
-                  <input type="text" placeholder='' className='outline-none bg-transparent ml-2 w-[350px]'/>
-             </fieldset>
 
              <fieldset className={fieldsetCss}> 
                 <legend className='ml-2 text-sm px-1'>Senha</legend>
